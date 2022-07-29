a2c2ab8eda51b266c2174aa0a3684bf7223fe991
HaileyMatz <hmatz525@gmail.com>
2022-07-29 15:17:32 -0700

    Initial Commit

---
+++ Vino mio/Vino_mioApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vino_mioApp.swift
+//  Vino mio
+//
+//  Created by Hailey Matzen on 7/29/22.
+//
+
+import SwiftUI
+
+@main
+struct Vino_mioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
